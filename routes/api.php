da7493630c457d7337be0b18e2fbed3897dfede4
arsyafarahman12327 <arsyafarah123@gmail.com>
2022-10-31 15:47:44 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    //return $request->user();
+    //});
+
+ Route::get('halo', function(){
+    return [
+        "me" => "Arsya"];
+ });
+
+ //Route::get('halocontroller', [HeloController::class,'index']);
+//Route::resource('halocontroller', HeloController::class);
+//Route::resource('siswacontroller', SiswaController::class);
+
+Route::resource('books', BookController::class)->except(
+    ['create','edit']
+);
